4429453befe792d8df5183ecdef846a21e7b22c2
Jonathan <dragonxxxxzzzz@gmail.com>
2021-04-21 17:07:36 -0300

    arregla error

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('hola');
 });
 
-app.listen(3000);
+app.listen(PORT);
